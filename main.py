5f262a3110a84d4c778cadfb9ba1bad4c8965601
woodooi <pavlo.antonyik2005@gmail.com>
2023-12-22 13:31:52 +0200

    dasdfasd

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from help.elems.event.handlers.event_reg_handler import event_registration_route
 from help.elems.event.handlers.event_output_handler import event_output_router
 from help.elems.user.handlers.user_reg_handler import registration_router
 from help.elems.user.handlers.user_profile_handler import profile_router
+from help.elems.event.handlers.event_search_handler import search_event
 from help.bot import bot
 
 
@@ -17,10 +18,13 @@ from help.elems.user.handlers.user_search_handler import search_musicians
 
 async def main():
     dp = Dispatcher()
+
+    dp.include_router(search_event)
     dp.include_router(registration_router)
     dp.include_router(event_registration_router)
     dp.include_router(search_musicians)
     dp.include_router(event_output_router)
+
     await set_commands(bot)
     dp.include_router(profile_router)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
